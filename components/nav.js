3fff03774f283e9ac4c6d66625a2f0a636e40eff
Tony Porter <tonyporter89@gmail.com>
2022-08-27 21:13:17 +0100

    bug fixes on console errors

--- components/nav.js
+++ components/nav.js
@@ -15,7 +15,7 @@ export default function Nav() {
 
     }, []);
     return (
-        <nav class="flex flex-wrap items-center justify-between w-full py-4 md:py-0 px-4 text-lg text-gray-700 bg-white">
+        <nav className="flex flex-wrap items-center justify-between w-full py-4 md:py-0 px-4 text-lg text-gray-700 bg-white">
             <div>
                 <a href="#">
                     <Image
@@ -30,15 +30,15 @@ export default function Nav() {
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 id="menu-button"
-                class="h-6 w-6 cursor-pointer md:hidden block"
+                className="h-6 w-6 cursor-pointer md:hidden block"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
             >
                 <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M4 6h16M4 12h16M4 18h16"
                 />
             </svg>
