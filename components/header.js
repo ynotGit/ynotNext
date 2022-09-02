809e2a1c99b92225e8bd3427df5085539c8ffec7
Tony Porter <tonyporter89@gmail.com>
2022-09-02 13:15:10 +0100

    bug fix on navbar

--- components/header.js
+++ components/header.js
@@ -8,16 +8,6 @@ export default function Nav() {
     const hamburgerLineTopBottom = `h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300`;
     const hamburgerLineMiddle = `h-1 w-4 my-1 ml-1.5 rounded-full bg-black transition ease transform duration-300`;
 
-    // useEffect(() => {
-    //     const button = document.querySelector('#menu-button');
-    //     const menu = document.querySelector('#menu');
-
-
-    //     button.addEventListener('click', () => {
-    //         menu.classList.toggle('hidden');
-    //     });
-
-    // }, []);
     return (
         <header>
             <nav className="flex flex-wrap items-center justify-between w-full py-4 md:py-0 md:py-4 text-lg text-gray-700 bg-white">
@@ -52,12 +42,12 @@ export default function Nav() {
                     />
                 </button>
 
-                {/* <div className="w-full px-4 md:flex md:items-center md:w-auto" id="menu"> */}
-                <div className={`${isOpen
-                    ? "max-h-30 w-full px-4 md:flex md:items-center md:w-auto"
-                    : "hidden max-h-0"
-                    }`} id="menu">
-                    <ul className="text-base text-gray-700 md:flex md:justify-between md:pt-0">
+                <div className="max-h-30 w-full px-4 md:flex md:items-center md:w-auto" id="menu">
+                    {/* <ul className="text-base text-gray-700 md:flex md:justify-between md:pt-0"> */}
+                    <ul className={`${isOpen
+                        ? "text-base text-gray-700 md:flex md:justify-between md:pt-0"
+                        : "hidden text-base text-gray-700 md:flex md:justify-between md:pt-0"
+                        }`} >
                         <li className="pl-4 md:pt-1  text-right">
                             <Link className="md:p-4 py-2 block hover:text-purple-400" href="#"
                             >About</Link>
